432ca598ce4bb10386a05751d45201e417ac1d70
vichy130 <viki_ccg@hotmail.com>
2023-08-26 22:54:41 -0600

    anadiendo hijos

--- js/form-ficha-clinica.js
+++ js/form-ficha-clinica.js
@@ -3,8 +3,28 @@
 
 let anadirHijo=document.getElementById("agregarHijo");
 
+class Hijo {
+    
+    constructor(id, sexo, edad){
+        this._id=id;
+        this._sexo= sexo;
+        this._edad= edad;
+    }
+    get id(){
+        return this._id;
+    }
+    get sexo(){
+        return this._sexo;
+    }
+    get edad(){
+        return this_edad;
+    }
+}
+
+
 function ingresarHijos (){
-alert("Ingresaste un hijo");
+    
+   let hijo= new Hijo();
 
 }
 anadirHijo.addEventListener("click", ingresarHijos);
