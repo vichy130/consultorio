589a83ced5ff5f3b607ee6bf9803b8784b4f9faf
vichy130 <viki_ccg@hotmail.com>
2024-02-16 23:24:02 -0600

    usuarios - validaciones v1

--- usuario.php
+++ usuario.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION['username'])) {
 }
 
 if (isset($_REQUEST["id"])) {
-    $_SESSION['id_usuario']=$_REQUEST["id"];
+    $_SESSION['id_usuario'] = $_REQUEST["id"];
 }
 ?>
 
@@ -45,55 +45,125 @@ if (isset($_REQUEST["id"])) {
 
         <form class="content-general formulario" id="form-usuario">
 
-            <label class="formulario_grupo span-4">Nuevo usuario</label>
+            <label class="formulario_grupo span-4">Usuario</label>
 
+            <!-- Grupo: Username -->
             <div class="formulario_grupo" id="grupo_usuario">
                 <label class="form_label" for="username-usuario">Nombre de usuario</label>
-                <input class="form_input" type="text" id="username-usuario" name="username-usuario">
+                <div class="form_grupo-input">
+                    <input class="form_input" type="text" id="username-usuario" name="username-usuario"
+                        placeholder="John123">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">El usuario tiene que ser de 4 a 16 dígitos y solo puede contener
+                    números, letras y guion bajo.</p>
             </div><!-- end form-grupo -->
 
-            <div class=" formulario_grupo">
+            <!-- Grupo: nombre -->
+            <div class=" formulario_grupo" id="grupo_nombre">
                 <label class="form_label" for="nombre-usuario">Nombre</label>
-                <input class="form_input" type="text" id="nombre-usuario" name="nombre-usuario">
+                <div class="form_grupo-input">
+                    <input class="form_input" type="text" id="nombre-usuario" name="nombre-usuario" placeholder="John">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">El nombre solo puede contener letras y no puede estar vacio.</p>
             </div><!-- end form-grupo -->
 
-            <div class="formulario_grupo">
+            <!-- Grupo: Apellido Paterno -->
+            <div class="formulario_grupo" id="grupo_apellidoPaterno">
                 <label class="form_label" for="apellidoPaterno-usuario">Apellido paterno</label>
-                <input class="form_input" type="text" id="apellidoPaterno-usuario" name="apellidoPaterno-usuario">
+                <div class="form_grupo-input">
+                    <input class="form_input" type="text" id="apellidoPaterno-usuario" name="apellidoPaterno-usuario"
+                        placeholder="Doe">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">El apellido solo puede contener letras y no puede estar vacio.</p>
             </div><!-- end form-grupo -->
 
-            <div class="formulario_grupo">
+            <!-- Grupo: Apellido materno -->
+            <div class="formulario_grupo" id="grupo_apellidoMaterno">
                 <label class="form_label" for="apellidoMaterno-usuario">Apellido materno</label>
-                <input class="form_input" type="text" id="apellidoMaterno-usuario" name="apellidoMaterno-usuario">
+                <div class="form_grupo-input">
+                    <input class="form_input" type="text" id="apellidoMaterno-usuario" name="apellidoMaterno-usuario">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">El apellido solo puede contener letras.</p>
             </div><!-- end form-grupo -->
 
-            <div class="formulario_grupo">
-                <label class="form_label" for="telefono-usuario">Telefono</label>
-                <input class="form_input" type="text" id="telefono-usuario" name="telefono-usuario">
+            <!-- Grupo: telefono -->
+            <div class="formulario_grupo" id="grupo_telefono">
+                <label class="form_label" for="telefono-usuario">Teléfono</label>
+                <div class="form_grupo-input">
+                    <input class="form_input" type="text" id="telefono-usuario" name="telefono-usuario" placeholder="33 3333 3333">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">El telefono solo puede contener números y el maximo son 14 dígitos.</p>
             </div><!-- end form-grupo -->
 
-            <div class="formulario_grupo">
-                <label class="form_label" for="correo-usuario">Correo electronico</label>
-                <input class="form_input" type="text" id="correo-usuario" name="correo-usuario">
+            <!-- Grupo: correo -->
+            <div class="formulario_grupo" id="grupo_correo">
+                <label class="form_label" for="correo-usuario">Correo electrónico</label>
+                <div class="form_grupo-input">
+                    <input class="form_input" type="text" id="correo-usuario" name="correo-usuario"
+                        placeholder="correo@correo.com">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">El correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
             </div><!-- end form-grupo -->
 
-            <div class="formulario_grupo">
-                <label class="form_label" for="contrasena-usuario">Contraseña</label>
-                <input class="form_input" type="text" id="contrasena-usuario" name="contrasena-usuario">
-            </div><!-- end form-grupo -->
-
-            <div class="formulario_grupo">
+            <!-- Grupo: tipo Usuario -->
+            <div class="formulario_grupo" id="grupo_tipo">
                 <label class="form_label" for="tipo-usuario">Tipo de usuario</label>
-                <input class="form_input" type="text" id="tipo-usuario" name="tipo-usuario">
+                <div class="form_grupo-input">
+                    <select name="tipo-usuario" id="tipo-usuario" class="form_input">
+                        <option value="">Selecciona una opción...</option>
+                        <option value="A">Administrador</option>
+                        <option value="M">Médico</option>
+                        <option value="S">Asistente</option>
+                    </select>
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">Selecciona un tipo de Usuario.</p>
             </div><!-- end form-grupo -->
-            <div class="formulario_grupo">
-                <label class="form_label" for="firma-usuario">Firma Médico</label>
-                <input class="form_input" type="file" id="firma-usuario" name="firma-usuario"
-                    accept=".jpg, .jpeg, .png">
-            </div><!-- end form-grupo -->
-            <button class="input_submit boton amarillo span-2">Imprimir</button>
-            <input class="input_submit boton azul span-2" type="submit" value="Guardar" id="boton-guardar-usuario">
 
+            <!-- Grupo: contrasena -->
+            <div class="formulario_grupo" id="grupo_contrasena">
+                <label class="form_label" for="contrasena-usuario">Contraseña</label>
+                <div class="form_grupo-input">
+                    <input class="form_input" type="password" id="contrasena-usuario" name="contrasena-usuario">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">La contraseña tiene que ser de 8 a 12 dígitos.</p>
+            </div><!-- end form-grupo -->
+
+            <!-- Grupo: contrasena 2 -->
+            <div class="formulario_grupo" id="grupo_contrasena">
+                <label class="form_label" for="contrasena-usuario"> Repetir contraseña</label>
+                <div class="form_grupo-input">
+                    <input class="form_input" type="password" id="contrasena-usuario2" name="contrasena-usuario2">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">Ambas contraseñas deben ser iguales.</p>
+            </div><!-- end form-grupo -->
+
+            <!-- Grupo: firma -->
+            <div class="formulario_grupo" id="grupo_firma">
+                <label class="form_label" for="firma-usuario">Firma Médico</label>
+                <div class="form_grupo-input">
+                    <input class="form_input" type="file" id="firma-usuario" name="firma-usuario"
+                        accept=".jpg, .jpeg, .png">
+                    <i class="form_validacion-estado fa-solid fa-circle-xmark"></i>
+                </div>
+                <p class="form_input-error">El archivo debe ser tipo JPG, JPEJ o PNG.</p>
+            </div><!-- end form-grupo -->
+
+            <div class="form_mensaje span-4" id="form_mensaje">
+                <p><i class="fa-solid fa-circle-exclamation"></i> Error: Porfavor rellena el formulario correctamente.</p>
+            </div>
+
+            <div class="formulario_grupo formulario_btn-enviar span-4">
+            <input class="input_submit boton azul" type="submit" value="Guardar" id="boton-guardar-usuario">
+            </div>
         </form>
         <!-- end FORM -->
         <?php require("./layout/footer.php"); ?>
